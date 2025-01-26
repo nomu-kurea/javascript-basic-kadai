47e16b3243f91932dcffe72f833f0f5048c778ce
nomu-kurea <1120nomu-kohei@ezweb.ne.jp>
2025-01-26 12:22:42 +0900

    Add files via upload

---
+++ kadai_010/holiday.js
@@ -0,0 +1,11 @@
+const holidays = ["正月", "成人の日", "建国記念の日", "春分の日", "昭和の日", "憲法記念日", "みどりの日", "こどもの日", "海の日", "山の日", "敬老の日", "勤労感謝の日", "天皇誕生日"];
+
+for (let i = 0; i < holidays.length; i++) {
+  console.log(holidays[i]);
+}
+
+let i = 0
+while(i < holidays.length) {
+    console.log(holidays[i]);
+    i++;
+}
