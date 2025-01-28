a22bcaaddf2c32e1de53e7ed2b6bac57e1e07ac8
nomu-kurea <1120nomu-kohei@ezweb.ne.jp>
2025-01-28 11:37:44 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("btn");
+
+const text = document.getElementById("text");
+
+btn.addEventListener("click", () => {
+
+ text.textContent = "ボタンをクリックしました。";
+
+
+ text.appendChild(clickList);
+
+});
